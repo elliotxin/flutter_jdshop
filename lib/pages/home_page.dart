48842da7329839744f7925a5036b2776bd3c12f9
elliot <elliot@alitec.sg>
2019-09-27 15:55:01 +0800

    add swiper

--- lib/pages/home_page.dart
+++ lib/pages/home_page.dart
@@ -1,5 +1,7 @@
+import 'dart:convert';
 import 'package:flutter/material.dart';
 import '../service/service_method.dart';
+import 'package:flutter_swiper/flutter_swiper.dart';
 
 class HomePage extends StatefulWidget {
   @override
@@ -10,23 +12,50 @@ class _HomePageState extends State<HomePage> {
   String homePageContent = '正在获取数据';
 
   @override
-  void initState() {
-    getHomePageContnet().then((val) {
-      setState(() {
-        homePageContent = val.toString();
-      });
-    });
-    super.initState();
-  }
-
-  @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        title: Text('百姓生活+'),
-      ),
-      body: SingleChildScrollView(
-        child: Text(homePageContent),
+        appBar: AppBar(
+          title: Text('百姓生活+'),
+        ),
+        body: FutureBuilder(
+          future: getHomePageContnet(),
+          builder: (context, snapshot) {
+            if (snapshot.hasData) {
+              var data = json.decode(snapshot.data.toString());
+              List<Map> swiper = (data['data']['slides'] as List).cast();
+              return Column(
+                children: <Widget>[
+                  SwiperDiy(swiperDateList: swiper),
+                ],
+              );
+            } else {
+              return Center(
+                child: Text('加载中'),
+              );
+            }
+          },
+        ));
+  }
+}
+
+//首页轮播组件
+class SwiperDiy extends StatelessWidget {
+  final List swiperDateList;
+  SwiperDiy({this.swiperDateList});
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      height: 333,
+      child: Swiper(
+        itemBuilder: (BuildContext context, int index) {
+          return Image.network(
+            '${swiperDateList[index]['image']}',
+            fit: BoxFit.fill,
+          );
+        },
+        itemCount: swiperDateList.length,
+        pagination: SwiperPagination(),
+        autoplay: true,
       ),
     );
   }
