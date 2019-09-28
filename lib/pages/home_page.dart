06d1212031203941b2b25af27180dd4998023f36
elliot <elliot@alitec.sg>
2019-09-28 14:36:15 +0800

    add product recommend

--- lib/pages/home_page.dart
+++ lib/pages/home_page.dart
@@ -15,6 +15,7 @@ class _HomePageState extends State<HomePage> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+        resizeToAvoidBottomInset: false,
         appBar: AppBar(
           title: Text('百姓生活+'),
         ),
@@ -31,16 +32,21 @@ class _HomePageState extends State<HomePage> {
                   data['data']['advertesPicture']['PICTURE_ADDRESS'];
               String leaderImage = data['data']['shopInfo']['leaderImage'];
               String leaderPhone = data['data']['shopInfo']['leaderPhone'];
-              return Column(
-                children: <Widget>[
-                  SwiperDiy(swiperDateList: swiper),
-                  TopNavigator(navigatorList: navigtorList),
-                  AdBanner(
-                    adPicture: adPicture,
-                  ),
-                  LeaderPhone(
-                      leaderImage: leaderImage, leaderPhone: leaderPhone),
-                ],
+              List<Map> recommendList =
+                  (data['data']['recommend'] as List).cast();
+              return SingleChildScrollView(
+                child: Column(
+                  children: <Widget>[
+                    SwiperDiy(swiperDateList: swiper),
+                    TopNavigator(navigatorList: navigtorList),
+                    AdBanner(
+                      adPicture: adPicture,
+                    ),
+                    LeaderPhone(
+                        leaderImage: leaderImage, leaderPhone: leaderPhone),
+                    Recommend(recommendList),
+                  ],
+                ),
               );
             } else {
               return Center(child: Text('加载中.......'));
@@ -146,5 +152,83 @@ class LeaderPhone extends StatelessWidget {
     } else {
       throw 'URL不能进行访问，异常';
     }
+  }
+}
+
+//商品推荐
+class Recommend extends StatelessWidget {
+  final List recommendList;
+  Recommend(this.recommendList);
+
+  //标题方法
+  Widget _titleWidget() {
+    return Container(
+      alignment: Alignment.centerLeft,
+      padding: EdgeInsets.fromLTRB(10, 2, 0, 5),
+      decoration: BoxDecoration(
+          color: Colors.white,
+          border:
+              Border(bottom: BorderSide(width: 0.5, color: Colors.black12))),
+      child: Text(
+        '商品推荐',
+        style: TextStyle(color: Colors.pink),
+      ),
+    );
+  }
+
+  //商品框方法
+  Widget _item(index) {
+    return InkWell(
+      onTap: () {},
+      child: Container(
+        height: ScreenUtil().setHeight(330),
+        width: ScreenUtil().setWidth(250),
+        padding: EdgeInsets.all(8),
+        decoration: BoxDecoration(
+            color: Colors.white,
+            border: Border(
+              left: BorderSide(width: 0.5, color: Colors.black12),
+            )),
+        child: Column(
+          children: <Widget>[
+            Image.network(recommendList[index]['image']),
+            Text('¥${recommendList[index]['mallPrice']}'),
+            Text('¥${recommendList[index]['price']}',
+                style: TextStyle(
+                  decoration: TextDecoration.lineThrough,
+                  color: Colors.green,
+                )),
+          ],
+        ),
+      ),
+    );
+  }
+
+  //横向列表
+  Widget _recommendList() {
+    return Container(
+      height: ScreenUtil().setHeight(330),
+      child: ListView.builder(
+        scrollDirection: Axis.horizontal,
+        itemCount: recommendList.length,
+        itemBuilder: (context, index) {
+          return _item(index);
+        },
+      ),
+    );
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      height: ScreenUtil().setHeight(400),
+      margin: EdgeInsets.only(top: 10),
+      child: Column(
+        children: <Widget>[
+          _titleWidget(),
+          _recommendList(),
+        ],
+      ),
+    );
   }
 }
